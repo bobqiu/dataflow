5ae31d40e18fab2c3bb5f1ae6285ddf54d63bacd
cuihailong <cuihailong3ai@gmail.com>
2018-01-23 21:04:01 +0800

    init

---
+++ src/main/java/com/qiu/dataflow/activiti/modeler/DispatcherServletConfiguration.java
@@ -0,0 +1,44 @@
+package com.qiu.dataflow.activiti.modeler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * Describe:
+ * Created by: bobqiu
+ * Date: 2018/1/22 下午1:52
+ */
+@Configuration
+@ComponentScan({"com.qiu.dataflow.activiti.rest.editor", "org.activiti.rest.diagram"})
+public class DispatcherServletConfiguration {
+    private final Logger log = LoggerFactory.getLogger(DispatcherServletConfiguration.class);
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        log.debug("Configuring localeChangeInterceptor");
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        log.debug("Creating requestMappingHandlerMapping");
+        RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
+        Object[] interceptors = {localeChangeInterceptor()};
+        requestMappingHandlerMapping.setInterceptors(interceptors);
+        return requestMappingHandlerMapping;
+    }
+}
